598b4d714a1490b97ce8f8b75c13e9aaee3d15ae
Dhairya Maisheri <dnm091298@gmail.com>
2026-02-21 13:02:41 +0530

    Turn debug off and add real Cloudinary key

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -33,7 +33,7 @@ DATABASES = {
 SECRET_KEY = "django-insecure-uu&l1*ol$(9wj&s!wayv@dow0_g%!%xo5dzg4p9ld=txl&=)u6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # settings.py
 ALLOWED_HOSTS = ['*']
